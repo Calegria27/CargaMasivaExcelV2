13232bc418714a8b3a8998054ddbcbdfef7d6072
Carlos <Carlos Alegria>
2023-07-06 12:04:42 -0400

    color modified

--- views/components/Navbar.jsx
+++ views/components/Navbar.jsx
@@ -1,39 +1,35 @@
-import { AppBar, Button, Stack, Toolbar } from "@mui/material";
-import { Box, Typography } from "@mui/material";
+import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
 import React, { useContext } from "react";
 import { Authcontext } from '../../Auth/context/Authcontext';
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    const { logout } = useContext(Authcontext);
-    const navigate = useNavigate();
-    const onLogout = () => {
-        logout();
-        navigate('/',);
-    }
+  const { logout } = useContext(Authcontext);
+  const navigate = useNavigate();
 
+  const onLogout = () => {
+    logout();
+    navigate('/');
+  }
 
-    return (
-        <AppBar position="sticky" sx={{ bgcolor: "skyblue" }}>
-            <Toolbar>
-                <Stack direction="row" justifyContent="space-between" alignContent="center">
-                    <Box sx={{ width: "20%", height: 1 / 4}}>
-                        <img style={{ width: "70%", height: "auto" }} src="https://www.cindependencia.cl/wp-content//themes/independencia/assets/img/logo-new-independencia.png" />
-                    </Box>
-                    <Box >
-                       <Typography variant="h2" color="white">Carga Masiva De Tarifados</Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Button variant="contained" color="error" onClick={onLogout}>Logout</Button>
-                    </Box>
-                </Stack>
-            </Toolbar>
-        </AppBar>
-
-
-
-    )
-
+  return (
+    <AppBar position="sticky" sx={{ bgcolor: "white" }}>
+      <Toolbar>
+        <Grid container alignItems="center">
+          <Grid item xs={12} sm={3} lg={2}>
+            <img style={{ width: "100%", height: "auto" }} src="https://www.cindependencia.cl/wp-content//themes/independencia/assets/img/logo-new-independencia.png" alt="Logo" />
+          </Grid>
+          <Grid item xs={12} sm={6} lg={8}>
+            <Typography variant="h2" align="center" sx={{ color: "#6D6D6D" }}>Carga Masiva De Tarifados</Typography>
+          </Grid>
+          <Grid item xs={12} sm={3} lg={2} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button variant="contained" color="error" onClick={onLogout}>Logout</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  )
 }
+
 
 export default Navbar;
